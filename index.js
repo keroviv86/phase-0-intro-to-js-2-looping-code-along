4e1d52a3a82cdaa22a4155bc1e9c4b08a2c258eb
Vivian Yim <keroviv86@gmail.com>
2021-11-26 02:25:12 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+let names = [ 'Lisa', 'Kaitlin', 'Jan' ]
+function writeCards(names, word){
+    let arr = []
+    for(let i=0; i<names.length; i++){
+        arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return arr
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num--)
+    }
+}
